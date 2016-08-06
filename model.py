e66afe7b41274f496d88a38b555ce3b6cbddf078
Tiya <yaleb.t@gmail.com>
2016-08-06 14:55:01 -0700

    Initial commit

--- model.py
+++ model.py
@@ -28,26 +28,28 @@ db = SQLAlchemy()
 #         return "<User user_id=%s first_name=%s last_name=%s contact_num=%s>" % (self.user_id, self.first_name, self.last_name, self.contact_num)
 
 
-# class Friend(db.Model):
-#     """Friend of Safebot User"""
+class FoodPrices(db.Model):
+    """Prices of Food"""
 
-#     __tablename__ = "friends"
+    __tablename__ = "foodprices"
 
-#     friend_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     first_name = db.Column(db.String(20), nullable=True)
-#     last_name = db.Column(db.String(30), nullable=True)
-#     contact_num = db.Column(db.Integer, nullable=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    food_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    country = db.Column(db.String(60), nullable=True)
+    city = db.Column(db.String(60), nullable=True)
+    market = db.Column(db.String(60), nullable=True)
+    commodity = db.Column(db.String(60), nullable=True)
+    priceform = db.Column(db.String(60), nullable=True)
+    quantityunit = db.Column(db.String(60), nullable=True)
+    month = db.Column(db.Integer, nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    price = db.Column(db.Integer, nullable=True)
+    commoditysource = db.Column(db.String(60), nullable=True)
 
-#     # Define relationships
-#     user = db.relationship('User')
+    def __repr__(self):
+        """Provide helpful representation when printed."""
 
-#     def __repr__(self):
-#         """Provide helpful representation when printed."""
-
-#         return "<Friend friend_id=%s first_name=%s last_name=%s contact_num=%s>" % (
-#             self.friend_id, self.first_name, self.last_name, self.contact_num)
-
+        return "<FoodPrices food_id=%s country=%s city=%s market=%s commodity=%s>" % (
+            self.food_id, self.country, self.city, self.market, self.commodity)
 
 # class Unsafe(db.Model):
 #     """Unsafe calls"""
@@ -70,7 +72,7 @@ db = SQLAlchemy()
 
 ##############################################################################
 
-def connect_to_db(app, db_uri="postgresql:///unbarelands"):
+def connect_to_db(app, db_uri="postgresql:///proactivesupply"):
     """Connect the database to our Flask app."""
 
     # Configure to use our PostgreSQL database
